4f3caee19fe4b26ca18f341f6c739c364aded02a
Ng Thian Ping <s10247447@connect.np.edu.sg>
2025-02-11 18:10:19 +0800

    testapp: indentation error

--- tests/testapp.py
+++ tests/testapp.py
@@ -32,7 +32,6 @@ class FlaskAppTestCase(unittest.TestCase):
                     ("admin_user", "adminpass"))
         cursor.execute("INSERT OR IGNORE INTO users (username, password, role) VALUES (?, ?, 'student')",
                     ("student_user", "studentpass"))
-                    ("student_user", "studentpass"))
         conn.commit()
         conn.close()
     
